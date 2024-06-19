49f1cbc0480dce0c1945181d9202da9eb5f993e8
Van-Hoang-Dev <vanhoang24edu@gmail.com>
2024-06-19 21:48:47 +0700

    Edit app js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,64 @@
     @yield('scripts')
     <script src='{{ asset('js/fullcalendar/index.global.min.js') }}'></script>
     <script src="{{ asset('js/app.js') }}"></script>
+    <script>
+        
+//_______________________Navbar___________________________________//
+const navbar = document.getElementById("navbar");
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Downscroll
+        navbar.classList.add("show");
+    } else if (scrollTop === 0) {
+        // Reached top of the page
+        navbar.classList.remove("show");
+    }
+    lastScrollTop = scrollTop;
+});
+
+function onToggleMenu() {
+    const sidebar = document.getElementById("sidebar");
+    const overlay = document.getElementById("overlay");
+
+    if (sidebar.classList.contains("open")) {
+        sidebar.classList.remove("open");
+        overlay.classList.remove("open");
+    } else {
+        sidebar.classList.add("open");
+        overlay.classList.add("open");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const closeButton = document.querySelector("#sidebar .close-btn");
+    closeButton.addEventListener("click", onToggleMenu);
+});
+//_______________________End Navbar___________________________________//
+
+
+//_______________________Calendar___________________________________//
+
+document.addEventListener("DOMContentLoaded", function () {
+    var calendarEl = document.getElementById("calendar");
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+         locale: 'vi',
+        initialView: "dayGridMonth",
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }
+    });
+    calendar.render();
+});
+
+
+//_______________________End Calendar___________________________________//
+
+    </script>
 </body>
 
 </html>
