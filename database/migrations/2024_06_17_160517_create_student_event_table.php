6248c20de6b7c1f1c4b7b9b02c7ebf594eabf4a5
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-06-20 13:45:04 +0700

    create model Student

--- database/migrations/2024_06_17_160517_create_student_event_table.php
+++ database/migrations/2024_06_17_160517_create_student_event_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('student_event', function (Blueprint $table) {
-            $table->integer('student_id');
+            $table->string('student_id');
             $table->integer('event_id');
             $table->primary(['student_id', 'event_id']);
             $table->timestamps();
--- resources/views/auths/login.blade.php
+++ resources/views/auths/login.blade.php
@@ -2,66 +2,63 @@
 
 {{-- Body --}}
 @section('content')
-    <div class="w-full h-[100vh] flex flex-col gap-3 items-center justify-center bg-gray-300">
-        {{-- Error notify --}}
-        @if ($errors->any())
-            <div class="mb-2 form_error_notify bg-white rounded-lg overflow-hidden">
-                <span class="block w-full p-4 bg-red-500 text-white">Thất bại</span>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li class="text-red-500 p-2">{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        {{-- Success notify --}}
-        @if (session('success'))
-            <div class="form_success_notify">
-                <div class="mb-2 form_error_notify bg-white rounded-lg overflow-hidden">
-                    <span class="block w-full p-4 bg-green-500 text-white">Thành công</span>
-                    <ul>
-                        <li class="text-green-500 p-4">{{ session('success') }}</li>
-                    </ul>
-                </div>
-            </div>
-        @endif
-        <form method="POST" action="{{ route('handle_login') }}"
-            class="min-w-[400px] shadow-lg overflow-hidden h-auto flex rounded-lg bg-gray-200 text-[var(--text)]  px-8 py-6 flex-col gap-4">
+<div class="w-full min-h-screen flex flex-col gap-3 items-center justify-center bg-gray-300">
+    {{-- Error notify --}}
+    @if ($errors->any())
+    <div class="mb-2 form_error_notify bg-white rounded-lg overflow-hidden">
+        <span class="block w-full p-4 bg-red-500 text-white">Thất bại</span>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li class="text-red-500 p-2">{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    {{-- Success notify --}}
+    @if (session('success'))
+    <div class="form_success_notify">
+        <div class="mb-2 form_error_notify bg-white rounded-lg overflow-hidden">
+            <span class="block w-full p-4 bg-green-500 text-white">Thành công</span>
+            <ul>
+                <li class="text-green-500 p-4">{{ session('success') }}</li>
+            </ul>
+        </div>
+    </div>
+    @endif
+    <div class="flex flex-col md:flex-row p-4 bg-gray-200 rounded-lg shadow-lg overflow-hidden w-full max-w-4xl">
+        <div class="w-full md:w-1/2 flex items-center justify-center">
+            <img src="{{ asset('assets/images/bg-login-1.jpg') }}" alt="" class="object-cover w-full h-full md:h-auto rounded-lg">
+        </div>
+
+        <form method="POST" action="{{ route('handle_login') }}" class="w-full md:w-1/2 flex flex-col gap-4 p-8">
             @csrf
             <h3 class="text-xl font-semibold">Đăng nhập với quyền quản trị</h3>
 
-            <label for="email" class="flex flex-col gap-[8px]">
+            <label for="email" class="flex flex-col gap-2">
                 <span class="text-sm text-gray-500">Email <span class="text-red-600">*</span></span>
-                <input type="email" placeholder="Vui lòng nhập email" name="email" id="email"
-                    class="rounded-lg border bg-transparent border-gray-500 p-2 outline-none w-full text-sm focus:border-blue-500 focus:border-2">
+                <input type="email" placeholder="Vui lòng nhập email" name="email" id="email" class="rounded-lg border bg-transparent border-gray-500 p-2 outline-none w-full text-sm focus:border-blue-500 focus:border-2">
             </label>
 
-            <label for="password" class="flex flex-col gap-[8px]">
+            <label for="password" class="flex flex-col gap-2">
                 <span class="text-sm text-gray-500">Mật khẩu <span class="text-red-600">*</span></span>
-                <input type="password" placeholder="Vui lòng nhập mật khẩu" name="password" id="password"
-                    class="rounded-lg border bg-transparent border-gray-500 p-2 outline-none w-full text-sm focus:border-blue-500 focus:border-2">
+                <input type="password" placeholder="Vui lòng nhập mật khẩu" name="password" id="password" class="rounded-lg border bg-transparent border-gray-500 p-2 outline-none w-full text-sm focus:border-blue-500 focus:border-2">
             </label>
 
-            <label for="remember" class="flex items-center justify-start gap-1">
+            <label for="remember" class="flex items-center gap-2">
                 <input type="checkbox" name="remember" id="remember" class="bg-[var(--background)]">
                 <span class="text-sm text-gray-500">Duy trì đăng nhập</span>
             </label>
 
-            <button
-                class="rounded-lg border text-sm text-gray-200 border-blue-500 bg-blue-400 hover:bg-blue-600 transition-colors duration-300 ease-in p-2 outline-none w-full">
+            <button class="rounded-lg border text-sm text-gray-200 border-blue-500 bg-blue-400 hover:bg-blue-600 transition-colors duration-300 ease-in p-2 outline-none w-full">
                 Đăng nhập
             </button>
         </form>
-
-        <div class="">
-            <p class="text-sm"><a href="/"
-                    class="text-blue-600">Trở về trang chủ</a></p>
-        </div>
     </div>
+    <div>
+        <p class="text-sm"><a href="/" class="text-blue-600">Trở về trang chủ</a></p>
+    </div>
+</div>
 @endsection
+
 @section('script')
-    <script>
-        // Your custom JavaScript here
-        console.log('Page loaded');
-    </script>
 @endsection
