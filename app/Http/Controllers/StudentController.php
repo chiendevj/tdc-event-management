1882263c180aa8ec154b491f3c426bea20590c5f
Datto098 <dat61222@gmail.com>
2024-06-22 17:44:25 +0700

    show students participant event

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -28,21 +28,20 @@ class StudentController extends Controller
         }
 
         // Lấy danh sách các sự kiện mà sinh viên đã tham gia
-         // Lấy danh sách các sự kiện mà sinh viên đã tham gia bằng truy vấn SQL
-         $events = DB::table('events')
-         ->join('event_student', 'events.id', '=', 'event_student.event_id')
-         ->where('event_student.student_id', $studentId)
-         ->select('events.*')
-         ->get();
+        // Lấy danh sách các sự kiện mà sinh viên đã tham gia bằng truy vấn SQL
+        $events = DB::table('events')
+            ->join('event_student', 'events.id', '=', 'event_student.event_id')
+            ->where('event_student.student_id', $studentId)
+            ->select('events.*')
+            ->get();
         // dd($events, $student);
 
         if ($events->isEmpty()) {
-        return response()->json(['student' => $student, 'events' => []]);
-            
+            return response()->json(['student' => $student, 'events' => []]);
         }
         return response()->json(['student' => $student, 'events' => $events]);
     }
-    
+
     /**
      * Show the form for creating a new resource.
      */
@@ -89,5 +88,35 @@ class StudentController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+
+    public function dashboard()
+    {
+        $students = Student::withCount('events')->orderByDesc('events_count')->paginate(20);
+        return view('dashboards.admin.students.index', compact('students'));
+    }
+
+    /**
+     * Get students by event count
+     */
+
+    public function getStudentsByEventCount()
+    {
+        $students = Student::withCount('events')->orderByDesc('events_count')->get();
+        return response()->json(["data" => $students, "status" => "success", "message" => "Get students by event count successfully!"]);
+    }
+
+    public function getStudentsById($id)
+    {
+        $student = Student::with(['events' => function ($query) {
+            $query->withCount('students as participants_count');
+        }])->find($id);
+
+        if (!$student) {
+            return response()->json(["data" => null, "status" => "error", "message" => "Student not found!"]);
+        }
+        $student->events_count = $student->events->count();
+        return response()->json(["data" => $student, "status" => "success", "message" => "Get student by id successfully!"]);
     }
 }
