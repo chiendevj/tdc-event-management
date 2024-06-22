8e93edb6ac80e065ea3f34213a152bf3225f3cf1
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-06-22 20:40:51 +0700

    doing

--- resources/views/dashboards/admin/events/show.blade.php
+++ resources/views/dashboards/admin/events/show.blade.php
@@ -2,18 +2,6 @@
 
 @section('title', 'Chi tiết')
 
-@section('head')
-    <!-- Open Graph Meta Tags for Facebook -->
-    <meta property="og:title" content="{{ $event->name }}" />
-    <meta property="og:description" content="{{ Str::limit(strip_tags($event->content), 100) }}" />
-    <meta property="og:url" content="{{ Request::fullUrl() }}" />
-    <meta property="og:type" content="article" />
-    <!-- If you have an event image -->
-    @if($event->image)
-        <meta property="og:image" content="{{ asset('storage/' . $event->image) }}" />
-    @endif
-@endsection
-
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/ckeditor.css') }}">
 <div class="w-full container mx-auto detail">
@@ -24,14 +12,8 @@
             </div>
         </div>
         <div class="col-span-2 p-4">
-            <!-- Facebook Share Button -->
-            <div class="fb-share-button" 
-                data-href="{{ Request::fullUrl() }}" 
-                data-layout="button" 
-                data-size="large">
-                <a target="_blank" 
-                   href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(Request::fullUrl()) }}" 
-                   class="fb-xfbml-parse-ignore">
+            <div class="fb-share-button" data-href="{{ $url }}" data-layout="button_count" data-size="large">
+                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u={{ $url }}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">
                     Chia sẻ
                 </a>
             </div>
@@ -39,21 +21,5 @@
     </div>
 </div>
 @endsection
-
 @section('scripts')
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0" nonce="YOUR_NONCE_VALUE"></script>
-
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var shareButton = document.querySelector('.fb-share-button a');
-        shareButton.addEventListener('click', function(event) {
-            event.preventDefault();
-            var url = shareButton.href;
-            window.open(url, 'facebook-share-dialog', 'width=800,height=600');
-            return false;
-        });
-    });
-</script>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,6 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Share Facbook meta -->
+    <meta property="og:url" content="{{ $url ?? '' }}" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="{{ $title ?? '' }}" />
+    <meta property="og:description" content="{{ $description ?? 'Sự kiện hấp dẫn' }}" />
+    <meta property="og:image" content="{{ $image ?? '' }}" />
     {{-- Fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -36,7 +42,7 @@
     @include('components.admin.footer')
     <script src="{{ asset('js/app.js') }}"></script>
     <div id="fb-root"></div>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v20.0&appId=760799009295495" nonce="MNU8X0yA"></script>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v20.0&appId=760799009295495" nonce="{{ $nonce ?? '' }}"></script>
 </body>
 
 </html>
