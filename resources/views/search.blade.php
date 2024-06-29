197e6d39fb5f178012ed09ab7e11b3ddd9e4b48a
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-06-29 17:58:29 +0700

    done

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,25 +3,23 @@
 @section('title', 'Tra cứu')
 
 @section('content')
-<div class="background-tdc">
-    <div class="search-box">
-        <h1 class="title"><span>Tra cứu</span><br> tham gia sự kiện</h1>
+<div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="bg-white shadow-md rounded-lg p-20 max-w-xl w-full">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-6 text-center">Tra cứu tham gia sự kiện</h1>
         <form id="searchForm" method="get" data-url="{{ route('search_events_by_student') }}">
-            <div class="flex items-center">
-                <input id="studentId" class="input-search rounded-l-lg" name="student_id" placeholder="Nhập mã số sinh viên" type="text">
-                <button type="submit" class="btn-search rounded-r-lg"><i class="fa-solid fa-magnifying-glass"></i> Tìm kiếm</button>
+            <div class="flex items-center mb-4 flex-col gap-5">
+                <input id="studentId" class="input-search flex-1 p-3 rounded-l-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" name="student_id" placeholder="Nhập mã số sinh viên" type="text">
+                <button type="submit" class="btn-search p-3 bg-blue-500 text-white rounded-r-lg hover:bg-blue-600"><i class="fa-solid fa-magnifying-glass"></i> Tìm kiếm</button>
             </div>
         </form>
         <!-- Modal -->
         <div id="eventsModal" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 hidden">
             <div class="bg-white rounded-lg w-full max-w-lg overflow-hidden">
-                <div class="flex justify-between items-center px-6 py-4">
+                <div class="flex justify-between items-center px-6 py-4 bg-blue-950">
                     <h2 class="text-lg font-semibold text-gray-200" id="modalTitle">Kết quả tra cứu</h2>
                     <button class="text-gray-200 hover:text-yellow-400 focus:outline-none p-5 text-2xl" onclick="closeModal()">&times;</button>
                 </div>
-                <div class="p-6" id="modalBody">
-
-                </div>
+                <div class="p-6" id="modalBody"></div>
             </div>
         </div>
     </div>
@@ -54,21 +52,13 @@
         var studentId = document.getElementById("studentId").value.trim();
 
         if (studentId === '') {
-            modalTitle.parentElement.classList.remove('bg-red-600', 'bg-blue-950')
-            modalTitle.parentElement.classList.add('bg-red-600')
-            modalTitle.innerHTML = 'Kết quả tra cứu';
-            modalBody.innerHTML = '<p class="text-gray-700" id="modalMessage">Vui lòng nhập mã số sinh viên để tra cứu.</p>';
-            modal.style.display = "flex";
+            showModal('bg-red-600', 'Vui lòng nhập mã số sinh viên để tra cứu.');
             return;  
         }
 
         // Kiểm tra mã số sinh viên với biểu thức chính quy
         if (!studentIdRegex.test(studentId)) {
-            modalTitle.parentElement.classList.remove('bg-red-600', 'bg-blue-950')
-            modalTitle.parentElement.classList.add('bg-red-600')
-            modalTitle.innerHTML = 'Kết quả tra cứu';
-            modalBody.innerHTML = 'Mã số sinh viên không hợp lệ.';
-            modal.style.display = "flex";
+            showModal('bg-red-600', 'Mã số sinh viên không hợp lệ.');
             return;  
         }
 
@@ -80,51 +70,63 @@
             })
             .then(function(data) {
                 if (data.error) {
-                    modalTitle.parentElement.classList.remove('bg-red-600', 'bg-blue-950')
-                    modalTitle.parentElement.classList.add('bg-red-600')
-                    modalTitle.innerHTML = 'Kết quả tra cứu';
-                    modalBody.innerHTML = '<p>' + data.error + '</p>';
-            } else {
-                modalTitle.parentElement.classList.remove('bg-red-600', 'bg-blue-950')
-                modalTitle.parentElement.classList.add('bg-blue-950')
-                modalTitle.innerHTML = 'Kết quả tra cứu cho sinh viên ' + data.student.fullname + ' (MSSV: ' + data.student.email.split('@')[0] + ')';
-
-                if (data.events.length > 0) {
-                    var html = '<h3 class="text-lg font-semibold mb-2">Các sự kiện sinh viên đã tham gia:</h3>';
-                    html += '<div class="overflow-x-auto">';
-                    html += '<table class="min-w-full divide-y divide-gray-200">';
-                    html += '<thead class="bg-gray-50">';
-                    html += '<tr>';
-                    html += '<th scope="col" class="px-6 py-3 text-left text-sm font-medium text-gray-200 bg-blue-800 uppercase tracking-wider">Tên sự kiện</th>';
-                    html += '</tr>';
-                    html += '</thead>';
-                    html += '<tbody class="bg-white divide-y divide-gray-200">';
-
-                    data.events.forEach(function(event, index) {
-                            var bgColorClass = index % 2 === 0 ? 'bg-gray-100' : 'bg-blue-400 text-white'; 
-
-                            html += '<tr class="' + bgColorClass + '">';
-                            html += '<td class="px-6 py-4 whitespace-nowrap"> <span class="font-medium">' + event.id + '. </span> ' + event.name + '</td>';
-                            html += '</tr>';
-                        });
-
-                    html += '</tbody>';
-                    html += '</table>';
-                    html += '</div>';
-
-                    modalBody.innerHTML = html;
+                    showModal('bg-red-600', data.error);
                 } else {
-                    modalBody.innerHTML = '<p>Sinh viên chưa tham gia sự kiện nào.</p>';
+                    showEvents(data);
                 }
-            }
-
-
-                modal.style.display = "flex";
-
             })
             .catch(function(error) {
                 console.error('Đã xảy ra lỗi:', error);
             });
+
+        function showModal(headerClass, message) {
+            modalTitle.parentElement.className = `flex justify-between items-center px-6 py-4 ${headerClass}`;
+            modalTitle.innerHTML = 'Kết quả tra cứu';
+            modalBody.innerHTML = `<p class="text-gray-700">${message}</p>`;
+            modal.style.display = "flex";
+        }
+
+        function showEvents(data) {
+            modalTitle.parentElement.className = 'flex justify-between items-center px-6 py-4 bg-blue-950';
+            modalTitle.innerHTML = `Kết quả tra cứu cho sinh viên ${data.student.fullname} (MSSV: ${data.student.id})`;
+
+            if (data.events.length > 0) {
+                let html = `
+                    <h3 class="text-lg font-semibold mb-2">Các sự kiện sinh viên đã tham gia:</h3>
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3 text-left text-sm font-medium text-gray-200 bg-blue-800 uppercase tracking-wider">Tên sự kiện</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                `;
+
+                data.events.forEach((event, index) => {
+                    const bgColorClass = index % 2 === 0 ? 'bg-gray-100' : 'bg-blue-400 text-white';
+                    html += `
+                        <tr class="${bgColorClass}">
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <span class="font-medium">${event.id}. </span> ${event.name}
+                            </td>
+                        </tr>
+                    `;
+                });
+
+                html += `
+                            </tbody>
+                        </table>
+                    </div>
+                `;
+
+                modalBody.innerHTML = html;
+            } else {
+                modalBody.innerHTML = '<p>Sinh viên chưa tham gia sự kiện nào.</p>';
+            }
+
+            modal.style.display = "flex";
+        }
     });
 </script>
 @endsection
