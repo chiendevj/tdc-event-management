635fddb2e69688da9f2d84b3213b1693e1e8930c
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-07-27 17:09:12 +0700

    fix ui header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,8 +3,7 @@
         <div>
             <img class="md:w-44 w-32 py-3 cursor-pointer" src="{{ asset('assets/logo/logo_fit_tdc.png') }}" alt="Logo FIT">
         </div>
-        <div class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] right-0 top-0 md:w-auto w-full flex flex-col items-start px-5 md:hidden gap-5"
-            id="sidebar">
+        <div class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] right-0 top-0 md:w-auto w-full flex flex-col items-start px-5 md:hidden gap-5" id="sidebar">
             <span class="close-btn self-end mt-3 cursor-pointer text-2xl">&times;</span>
             <ul class="nav-menu flex md:flex-row flex-col md:items-center w-full mt-4 gap-[20px]">
                 <li class="{{ Route::is('home') ? 'active' : '' }}">
@@ -16,9 +15,15 @@
                 <li class="{{ Route::is('tra-cuu') ? 'active' : '' }}">
                     <a class="hover:text-gray-500 font-semibold" href="{{ route('tra-cuu') }}">Tra cứu</a>
                 </li>
+                @if (!Auth::check())
                 <li>
                     <a class="hover:text-gray-500 font-semibold" href="{{ route('login') }}">Đăng nhập</a>
                 </li>
+                @else
+                <li>
+                    <a class="hover:text-gray-500 font-semibold" href="{{ route('login') }}">Quản trị viên</a>
+                </li>
+                @endif
 
             </ul>
         </div>
